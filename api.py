962c9068c5be01105e51630650b8316c5ec8a01b
Matěj <mates.sveda@gmail.com>
2019-02-11 11:31:27 +0100

    splitting code, exceptions and tests

--- api.py
+++ api.py
@@ -1,17 +1,18 @@
 from flask import Flask, request
-from converter import convert
+from converter import convert_to_all, convert_between
 
 app = Flask(__name__)
 
 @app.route('/convert', methods=['GET'])
 def get():
-    # Creates arguments
     amount = request.args.get('amount', type=float)
     input_currency = request.args.get('input_currency')
     output_currency = request.args.get('output_currency', default=None)
 
-    # Returns convert function from converter.py
-    return convert(amount, input_currency, output_currency)
+    if output_currency is None:
+        return convert_to_all(amount, input_currency)
+    else:
+        return convert_between(amount, input_currency, output_currency)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- converter.py
+++ converter.py
@@ -6,108 +6,112 @@ def error_msg(type, msg):
     err = {type: msg}
     return json.dumps(err)
 
-def convert(amount, input_currency, output_currency=None):
-    input_currency = input_currency.upper()
-    output_currency = output_currency.upper()
-    # Loads local file and creates a list of available currencies
-    with open('currencies_supported.json') as file:
-        data = json.load(file)
-        currencies = []
-        for currency in data:
-            currencies.append(currency['cc'])
-            currencies.append(currency['symbol'])
-            
+def local_currency_file():
+    with open('currencies_supported.json') as data:
+        return json.load(data)
+
+def check_currency(user_input):
     try:
-        if type(amount) not in [int, float] or amount <= 0:
-            raise RuntimeError
+        result = [curr for curr in local_currency_file() if curr['cc'] == user_input or curr['symbol'] == user_input]
+        if result != []:
+            return result[0]['cc']
+        else:
+            raise NameError
     except:
-        return error_msg('ValueError', "Amount has to be a number greater than zero!")
+        return error_msg('Error', "Entered currency code or symbol not found! Mind the upper casing!")
 
+def web_scrape_all_rates(amount, input_currency):
     try:
-        if input_currency not in currencies or output_currency not in currencies:
-            if output_currency is None and input_currency in currencies:
-                pass
-            else:
-                raise NameError
-    except:
-        return error_msg('NameError', "Entered currency code or symbol not found! Mind the letter casing!")
-
-    # Case executed without output_currency argument
-    if output_currency is None:
-        # Changes currency symbol for currency code
-        for currency in data:
-            if input_currency == currency['symbol']:
-                input_currency = currency['cc']
-
         url = 'https://www.xe.com/currencytables/?from={}'.format(input_currency)
         source = requests.get(url).text
-        # Passes HTML into BeautifulSoup
         soup = BeautifulSoup(source, 'lxml')
+    except:
+        return error_msg('Error', "Exchange website not responding, please check your connection!")
 
-        # Scrapes currency rate
-        table_scraped = soup.find('table', id='historicalRateTbl')
-        rows = table_scraped.select('tr')
-        output = {}
-        for cur in rows:
-            cols = cur.find_all('td')
-            if len(cols) > 0:
-                output[cols[0].next.next] = float(cols[2].next)
+    # Scrapes currency rate
+    table_scraped = soup.find('table', id='historicalRateTbl')
+    rows = table_scraped.select('tr')
+    rates = {}
+    for cur in rows:
+        cols = cur.find_all('td')
+        if len(cols) > 0:
+            rates[cols[0].next.next] = float(cols[2].next)
 
-        # Multiplies rates by amount and round
-        for key, value in output.items():
-            output[key] = round(value * amount, 2)
+    # Multiplies rates by amount and round
+    for key, value in rates.items():
+        rates[key] = round(value * amount, 2)
+    return rates
 
-        result = {
-            "input": {
-                "amount": amount,
-                "currency": input_currency
-            },
-
-            "output": output
-        }
-
-        return json.dumps(result, sort_keys=True)
-
-    # Case executed without output_currency argument
-    else:
-        # Changes currency symbol for currency code
-        for currency in data:
-            if currency['symbol'] == input_currency:
-                input_currency = currency['cc']
-            if currency['symbol'] == output_currency:
-                output_currency = currency['cc']
-
-        url = 'https://www.kurzy.cz/kurzy-men/kurzy.asp?a=X&mena1={}&mena2={}&c={}&d=27.6.2011&convert=P%F8eve%EF+m%ECnu'\
+def web_scrape_rate(amount, input_currency, output_currency):
+    try:
+        url = 'https://www.kurzy.cz/kurzy-men/kurzy.asp?a=X&mena1={}&mena2={}&c={}&d=latest&convert=P%F8eve%EF+m%ECnu' \
             .format(input_currency, output_currency, amount)
         source = requests.get(url).text
+    except:
+        return error_msg('Error', "Exchange website not responding, please check your connection!")
 
-        # Passes HTML into BeautifulSoup
-        soup = BeautifulSoup(source, 'lxml')
+    soup = BeautifulSoup(source, 'lxml')
 
-        # Srapes currency rate
-        table_scraped = soup.find_all('span', attrs={'class': 'result'})
-        output_str = (table_scraped[1].text)
+    table_scraped = soup.find_all('span', attrs={'class': 'result'})
+    output_str = (table_scraped[1].text)
 
-        # Deletes whitespaces if needed
-        if " " in output_str:
-            output_str = output_str.replace(" ", "")
+    if " " in output_str:
+        output_str = output_str.replace(" ", "")
 
-        # Parses output_str to float and round rate
-        output = float(output_str)
-        output = round(output, 2)
+    rate = float(output_str)
+    rate = round(rate, 2)
+    return rate
 
+def convert_between(amount, input_currency, output_currency):
+    # Check amount type and value
+    try:
+        if type(amount) not in [int, float] or amount <= 0:
+            raise ValueError
+    except:
+        return error_msg('Error', "Amount has to be a number greater than zero!")
+
+    input_currency_checked = check_currency(input_currency)
+    output_currency_checked = check_currency(output_currency)
+
+    # If currency 3-letter code not found, return an error message defined in check_currency()
+    if len(input_currency_checked) != 3:
+        return input_currency_checked
+    elif len(output_currency_checked) != 3:
+        return output_currency_checked
+    else:
         result = {
             "input": {
                 "amount": amount,
-                "currency": input_currency
+                "currency": input_currency_checked
             },
 
             "output": {
-                output_currency: output
+                output_currency_checked: web_scrape_rate(amount, input_currency_checked, output_currency_checked)
             }
         }
-
-        # Returns json result
         return json.dumps(result, sort_keys=True)
 
-convert(10, "USD", "EUR")
+def convert_to_all(amount, input_currency):
+    # Check amount type and value
+    try:
+        if type(amount) not in [int, float] or amount <= 0:
+            raise ValueError
+    except:
+        return error_msg('Error', "Amount has to be a number greater than zero!")
+
+    input_currency_checked = check_currency(input_currency)
+
+    # If currency 3-letter code not found, return an error message defined in check_currency()
+    if len(input_currency_checked) != 3:
+        return input_currency_checked
+
+    else:
+        result = {
+            "input": {
+                "amount": amount,
+                "currency": input_currency_checked
+            },
+
+            "output": web_scrape_all_rates(amount, input_currency_checked)
+        }
+        return json.dumps(result, sort_keys=True)
